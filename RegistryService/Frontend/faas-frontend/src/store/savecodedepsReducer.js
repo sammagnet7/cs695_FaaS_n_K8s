17ca78b59841113a124572c550516385a9677b13
ArifAli-0 <warrobotrocx@gmail.com>
2024-04-20 18:36:57 +0530

    Adds Faas Frontend

---
+++ RegistryService/Frontend/faas-frontend/src/store/savecodedepsReducer.js
@@ -0,0 +1,31 @@
+// project imports
+import config from '../config';
+
+// action - state management
+import * as actionTypes from './actions';
+
+export const initialState = {
+    code: '',
+    deps: '',
+    runtime: 'python'
+};
+
+// ==============================|| CUSTOMIZATION REDUCER ||============================== //
+
+const savecodedepsReducer = (state = initialState, action) => {
+    let id;
+    switch (action.type) {
+        case actionTypes.SAVE_CODE_DEPS:
+            console.log(action)
+            return {
+                ...state,
+                code: action.code,
+                deps: action.deps,
+                runtime: action.runtime
+            };
+        default:
+            return state;
+    }
+};
+
+export default savecodedepsReducer;
